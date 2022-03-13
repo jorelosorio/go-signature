d9840d9cc24ee246a6291f14f1084d5ae72a3ca3
Jorge Osorio <jorelosorio@gmail.com>
2022-03-13 21:03:58 +0100

    Update pack.go

--- helpers/pack.go
+++ helpers/pack.go
@@ -11,7 +11,7 @@ import (
 	pb "signatures-playground/structspb"
 )
 
-func PackAndSignMessage(message *pb.Message, key *structs.AsymmetricKey) []byte {
+func PackAndSignMessage(message *pb.Message, key *structs.AsymmetricKey) (signature []byte, encodedContainer []byte) {
 	// Before signing, we need to hash our message
 	// The hash is what we actually sign
 	hash := sha256.New()
@@ -25,17 +25,17 @@ func PackAndSignMessage(message *pb.Message, key *structs.AsymmetricKey) []byte 
 
 	hashSum := hash.Sum(nil)
 
-	signatureBytes, err := rsa.SignPKCS1v15(rand.Reader, key.PrivateKey, crypto.SHA256, hashSum)
+	signature, err = rsa.SignPKCS1v15(rand.Reader, key.PrivateKey, crypto.SHA256, hashSum)
 
 	if err != nil {
 		fmt.Printf("Failed to sign message: %s", err)
 		os.Exit(1)
 	}
 
-	container := &pb.Container{Message: message, Signature: signatureBytes}
-	containerBytes := EncodeContainer(container)
+	container := &pb.Container{Message: message, Signature: signature}
+	encodedContainer = EncodeContainer(container)
 
-	return containerBytes
+	return
 }
 
 func IsAuthentic(container *pb.Container, key *structs.AsymmetricKey) bool {
